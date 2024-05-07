c21182bce6c725d92826df01a345f317a1f48c5a
Noel Simbolon <84700640+noelsimbolon@users.noreply.github.com>
2024-05-08 01:10:55 +0700

    refactor orb prefab and behaviour class name

--- Assets/Scripts/Orb/OrbBehaviour.cs
+++ Assets/Scripts/Orb/OrbBehaviour.cs
@@ -1,9 +1,6 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class OrbsBehaviourScript : MonoBehaviour
+public abstract class OrbBehaviour : MonoBehaviour
 {
     public float countdownDuration = 5f;
     public float detectionRadius = 5f;
@@ -11,33 +8,41 @@ public abstract class OrbsBehaviourScript : MonoBehaviour
 
     private bool used;
 
-    private void Start()
+    public void Start()
     {
         Invoke(nameof(RemoveOrb), countdownDuration);
     }
 
-    protected void RemoveOrb()
+    public void RemoveOrb()
     {
         if (used)
         {
             return;
         }
+
         Destroy(gameObject);
     }
 
     private void Update()
     {
+        // If the player is not colliding with the orb...
         Collider[] hitColliders = Physics.OverlapSphere(transform.position, detectionRadius, playerLayerMask);
         if (hitColliders.Length != 1)
         {
+            // Return the method
             return;
         }
 
+        // If the player is colliding:
+        // 1. Execute the orb perk
+        // 2. Mark it as used
+        // 3. Remove it
         PlayerManager playerManager = hitColliders[0].GetComponentInParent<PlayerManager>();
-        PlayerEnter(playerManager);
+        PlayerMovement playerMovement = hitColliders[0].GetComponentInParent<PlayerMovement>();
+        ExecuteOrbPerk(playerManager, playerMovement);
         used = true;
         RemoveOrb();
     }
 
-    protected abstract void PlayerEnter(PlayerManager playerManager);
+    protected abstract void ExecuteOrbPerk(PlayerManager playerManager, PlayerMovement playerMovement);
 }
