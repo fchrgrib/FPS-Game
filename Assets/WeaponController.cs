75325898deb98c1286945c46ab856f4f673d55af
Fahrian Afdholi <89321009+fchrgrib@users.noreply.github.com>
2024-05-07 23:55:15 +0700

    feat: add scroll wheel controller

--- Assets/WeaponController.cs
+++ Assets/WeaponController.cs
@@ -8,16 +8,47 @@ public class WeaponController : MonoBehaviour
     public GameObject shotgun;
     public GameObject sword;
 
-    bool previousStep;
+    int currentWeapon = 1;
+    float scrollWheel;
+    List<GameObject> weapons = new List<GameObject>();
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        weapons.Add(gun);
+        weapons.Add(shotgun);
+        weapons.Add(sword);
+    }
+
+    void ChangeWeapon()
+    {
+        for (int i = 0; i < weapons.Count; i++)
+        {
+            weapons[i].SetActive((i + 1) == currentWeapon);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        float scrollWheel = Input.GetAxis("Mouse ScrollWheel");
+
+        if (scrollWheel > 0)
+        {
+            currentWeapon = (currentWeapon + 1) % 3 + 1;
+            ChangeWeapon();
+
+        }
+
+        if (scrollWheel < 0)
+        {
+            currentWeapon -= 1;
+            if (currentWeapon < 1)
+            {
+                currentWeapon = 3;
+            }
+            ChangeWeapon();
+        }
+
         if (Input.GetKeyUp(KeyCode.Alpha1))
         {
             shotgun.SetActive(false);
