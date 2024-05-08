d63fd90737f4880e55f90f7f910d419759cb9896
Ammar Rasyad Chaeroel <ammarasyad@gmail.com>
2024-05-09 01:17:49 +0700

    Add jenderal damage behavior

--- Assets/Scripts/EnemyR/JenderalBehavior.cs
+++ Assets/Scripts/EnemyR/JenderalBehavior.cs
@@ -1,24 +1,49 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 namespace Nightmare
 {
     public class JenderalBehavior : MonoBehaviour
     {
-        public float distanceThreshold = 10f;
+        public float distanceThreshold = 5f;
         public float damagePerSecond = 3f;
-        private GameObject player;
+        private Transform player;
+        private PlayerManager playerManager;
+
+        private bool coroutineStarted;
         
         void Awake()
         {
-            player = GameObject.FindGameObjectWithTag("Player");
+            player = GameObject.Find("PlayerOnly").transform;
+            playerManager = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerManager>();
         }
 
         void Update()
         {
-            if (Vector3.Distance(player.transform.position, transform.position) < distanceThreshold)
+            if (coroutineStarted) return;
+            
+            if (WithinDistance())
             {
-                // Do something
+                StartCoroutine(nameof(DealDamageToPlayer));
+                coroutineStarted = true;
+            }
+        }
+
+        private bool WithinDistance() => Vector3.Distance(player.transform.position, transform.position) < distanceThreshold;
+
+        private IEnumerator DealDamageToPlayer()
+        {
+            while (true) {
+                yield return new WaitForSeconds(1f);
+                playerManager.TakeDamage(damagePerSecond);
+
+                if (!WithinDistance())
+                {
+                    print("stopped");
+                    coroutineStarted = false;
+                    break;
+                }
             }
         }
     }
