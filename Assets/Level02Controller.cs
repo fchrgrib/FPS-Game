6547b2d0ea999f4e15326585f921ad4eeade5eee
Fahrian Afdholi <89321009+fchrgrib@users.noreply.github.com>
2024-05-11 05:03:55 +0700

    feat: add mission level 02

--- Assets/Level02Controller.cs
+++ Assets/Level02Controller.cs
@@ -6,6 +6,8 @@ public class Level02Controller : MonoBehaviour
 {
     public int EnemyDeathCount { get; private set; }
     public int EnemyLeaderDeathCount { get; private set; }
+    public int maxKerocoDeath = 5;
+    public int maxLeaderOfKerocoDeath = 4;
 
     public GameObject finalBox;
     public GameObject finalGate;
@@ -16,6 +18,7 @@ public class Level02Controller : MonoBehaviour
     {
         EventManager.StartListening("EnemyDeath", IncrementEnemyDeathCount);
         EventManager.StartListening("LeaderOfEnemyDeath", IncrementLeaderOfEnemyDeathCount);
+        missionText.SetText(SetTextMission());
     }
 
     private void OnDestroy()
@@ -24,20 +27,27 @@ public class Level02Controller : MonoBehaviour
         EventManager.StopListening("LeaderOfEnemyDeath", IncrementLeaderOfEnemyDeathCount);
     }
 
+    string SetTextMission()
+    {
+        return $"Your Mission\nKill Keroco     {EnemyDeathCount}/{maxKerocoDeath}\nKill Kepala Keroco   {EnemyLeaderDeathCount}/{maxLeaderOfKerocoDeath}";
+    }
+
     private void IncrementEnemyDeathCount()
     {
         EnemyDeathCount++;
+        missionText.SetText(SetTextMission());
     }
 
     private void IncrementLeaderOfEnemyDeathCount()
     {
         EnemyLeaderDeathCount++;
+        missionText.SetText(SetTextMission());
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (EnemyLeaderDeathCount >= 3)
+        if (EnemyLeaderDeathCount >= 4 && EnemyDeathCount>=5)
         {
             finalBox.SetActive(true);
             finalGate.SetActive(false);   
