0ae02c3348d3876182324d21792cc0bbdb999e42
Fahrian Afdholi <89321009+fchrgrib@users.noreply.github.com>
2024-05-11 05:30:34 +0700

    feat: add mission text level 04

--- Assets/Level04Controller.cs
+++ Assets/Level04Controller.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Level04Controller : MonoBehaviour
@@ -8,6 +9,11 @@ public class Level04Controller : MonoBehaviour
     public int EnemyLeaderDeathCount { get; private set; }
     public int EnemyAdmiralDeathCount { get; private set; }
     public int EnemyKingDeathCount { get; private set; }
+    public TMP_Text missionText;
+
+    public int maxLeaderKerocoDeath = 2;
+    public int maxAdmiralOfKerocoDeath = 2;
+    public int maxKingOfKerocoDeath = 1;
     
     // Start is called before the first frame update
     void Start()
@@ -16,6 +22,7 @@ public class Level04Controller : MonoBehaviour
         EventManager.StartListening("LeaderOfEnemyDeath", IncrementLeaderOfEnemyDeathCount);
         EventManager.StartListening("AdmiralOfEnemyDeath", IncrementAdmiralOfEnemyDeathCount);
         EventManager.StartListening("KingOfEnemyDeath", IncrementKingOfEnemyDeathCount);
+        missionText.SetText(SetTextMission());
     }
     
     private void OnDestroy()
@@ -25,10 +32,16 @@ public class Level04Controller : MonoBehaviour
         EventManager.StopListening("AdmiralOfEnemyDeath", IncrementAdmiralOfEnemyDeathCount);
         EventManager.StopListening("KingOfEnemyDeath", IncrementKingOfEnemyDeathCount);
     }
+
+    string SetTextMission()
+    {
+        return $"Your Mission\nKill Kepala Keroco   {EnemyLeaderDeathCount}/{maxLeaderKerocoDeath}\nKill Jenderal     {EnemyAdmiralDeathCount}/{maxAdmiralOfKerocoDeath}\nKill King     {EnemyKingDeathCount}/{maxKingOfKerocoDeath}";
+    }
     
     private void IncrementEnemyDeathCount()
     {
         EnemyDeathCount++;
+        missionText.SetText(SetTextMission());
     }
 
     private void IncrementLeaderOfEnemyDeathCount()
@@ -39,17 +52,19 @@ public class Level04Controller : MonoBehaviour
     private void IncrementAdmiralOfEnemyDeathCount()
     {
         EnemyAdmiralDeathCount++;
+        missionText.SetText(SetTextMission());
     }
 
     private void IncrementKingOfEnemyDeathCount()
     {
         EnemyKingDeathCount++;
+        missionText.SetText(SetTextMission());
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (EnemyKingDeathCount>0)
+        if (EnemyKingDeathCount>=maxKingOfKerocoDeath && EnemyAdmiralDeathCount>=maxAdmiralOfKerocoDeath && EnemyLeaderDeathCount >= maxLeaderKerocoDeath)
         {
             //TODO: Do something if king of enemy die  
         }
