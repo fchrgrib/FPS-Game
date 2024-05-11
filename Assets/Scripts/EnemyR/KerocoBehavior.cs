815babb3202e3b76e3c6ee8e107fb2ba72460b90
Fahrian Afdholi <89321009+fchrgrib@users.noreply.github.com>
2024-05-11 14:01:54 +0700

    feat: add daamge keroco

---
+++ Assets/Scripts/EnemyR/KerocoBehavior.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using UnityEngine;
+public class KerocoBehavior: MonoBehaviour
+{
+    public float distanceThreshold = 10f;
+    public float damagePerSecond = 1f;
+    protected GameObject Player;
+    protected PlayerManager PlayerManager;
+
+    protected bool CoroutineStarted;
+    
+    protected virtual void Awake()
+    {
+        Player = GameObject.Find("PlayerOnly");
+        PlayerManager = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerManager>();
+    }
+    
+    void OnDestroy()
+    {
+        StopCoroutine(nameof(ApplyToPlayer));
+    }
+
+    void Update()
+    {
+        if (CoroutineStarted) return;
+        
+        if (WithinDistance())
+        {
+            StartCoroutine(nameof(ApplyToPlayer));
+            CoroutineStarted = true;
+        }
+    }
+
+    private bool WithinDistance() => Vector3.Distance(Player.transform.position, transform.position) < distanceThreshold;
+
+    protected IEnumerator ApplyToPlayer()
+    {
+        while (true) {
+            yield return new WaitForSeconds(1f);
+            ApplyEffectsToPlayer();
+
+            if (!WithinDistance())
+            {
+                DispelEffects();
+                CoroutineStarted = false;
+                break;
+            }
+        }
+    }
+
+    protected virtual void ApplyEffectsToPlayer()
+    {
+        PlayerManager.TakeDamage(damagePerSecond);
+    }
+
+    protected virtual void DispelEffects()
+    {
+        
+    }    
+}
