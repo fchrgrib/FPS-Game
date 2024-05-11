098974d76328cc5b8e0c487670119f55178761e4
Zidane <13521163@std.stei.itb.ac.id>
2024-05-11 14:01:55 +0700

    add difficulty

--- Assets/Scripts/Menu/Option.cs
+++ Assets/Scripts/Menu/Option.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Nightmare.Enum;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -13,6 +14,25 @@ public class Option : MonoBehaviour
     public void changePlayerName(TMP_InputField tmpInputField)
     {
         playerName = tmpInputField.text;
+    }
+
+    public void ChangeDifficulty(int diff)
+    {
+        switch (diff)
+        {
+            case 0:
+                MobSpawner.Difficulty = Difficulty.Easy;
+                break;
+            case 1:
+                MobSpawner.Difficulty = Difficulty.Medium;
+                break;
+            case 2:
+                MobSpawner.Difficulty = Difficulty.Hard;
+                break;
+            default:
+                MobSpawner.Difficulty = Difficulty.Easy;
+                break;
+        }
     }
 
     public void OptionMenu()
